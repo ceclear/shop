54e3d5269b272c735ddb7b9d41fe8b4d86c765a3
ceclear <594652523@qq.com>
2021-02-18 17:55:57 +0800

    wechat223

--- app/Http/Middleware/jwt.php
+++ app/Http/Middleware/jwt.php
@@ -12,14 +12,14 @@ class jwt
     public function handle($request, Closure $next)
     {
         if (!$this->getToken()) {
-            return response()->json(['code' => 1, 'message' => '缺少身份认证参数']);
+            return response()->json(['code' => -1, 'message' => '缺少身份认证参数']);
         }
         if (!$this->jwtToken()->validate()) {
-            return response()->json(['code' => 2, 'message' => '登录状态失效']);
+            return response()->json(['code' => -1, 'message' => '登录状态失效']);
         }
         $payload = $this->jwtPayload();
         if ($payload['exp'] <= time()) {
-            return response()->json(['code' => 2, 'message' => '登录已过期']);
+            return response()->json(['code' => -1, 'message' => '登录已过期']);
         }
         $request->uid = $payload['uid'];
         return $next($request);
