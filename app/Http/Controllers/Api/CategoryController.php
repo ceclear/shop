3004dfaa8ac9a8a84d52a19d6afd996f11d34e71
ceclear <594652523@qq.com>
2021-02-19 16:01:10 +0800

    wechat23

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Traits\ResponseJson;
 
 class CategoryController extends Controller
 {
 
+    use ResponseJson;
+
     public function lists()
     {
         $categoryList = Category::getCategory();
-        return response()->json($categoryList);
+        return $this->responseJson(0, '', $categoryList);
     }
 }
