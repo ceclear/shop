8088d1aa34d716a1c6774146653bb5ccd9a414b5
ceclear <594652523@qq.com>
2021-03-03 14:36:32 +0800

    cache index data

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Advert;
 use App\Models\Goods;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
@@ -12,14 +13,31 @@ class IndexController extends Controller
 
     public function index()
     {
-        $field    = ['id', 'sku', 'discover', 'title', 'brand_code', 'discount', 'images', 'is_new', 'price', 'discount_price', 'star'];
-        $newList  = Goods::where('status', 1)->where('is_new', 1)->limit(6)->orderBy(DB::raw('RAND()'))->get($field)->toArray();
-        $bestList = Goods::where('status', 1)->limit(6)->orderBy('sale', 'desc')->get($field)->toArray();
-        $recList  = Goods::where('status', 1)->where('is_recommend', 1)->limit(6)->orderBy(DB::raw('RAND()'))->get($field)->toArray();
-        $saleOff  = Goods::where('discount', '>', 0)->where('status', 1)->orderBy('id', 'desc')->limit(2)->get($field)->toArray();
-        $banner   = Advert::with(["ad_position"=>function($query){
-            $query->where('symbol', 'sybanner');
-        }])->where('status', 1)->get()->toArray();
+        $field = ['id', 'sku', 'discover', 'title', 'brand_code', 'discount', 'images', 'is_new', 'price', 'discount_price', 'star'];
+//        $newList  = Goods::where('status', 1)->where('is_new', 1)->limit(6)->orderBy(DB::raw('RAND()'))->get($field)->toArray();
+//        $bestList = Goods::where('status', 1)->limit(6)->orderBy('sale', 'desc')->get($field)->toArray();
+//        $recList  = Goods::where('status', 1)->where('is_recommend', 1)->limit(6)->orderBy(DB::raw('RAND()'))->get($field)->toArray();
+//        $saleOff  = Goods::where('discount', '>', 0)->where('status', 1)->orderBy('id', 'desc')->limit(2)->get($field)->toArray();
+//        $banner   = Advert::with(["ad_position" => function ($query) {
+//            $query->where('symbol', 'sybanner');
+//        }])->where('status', 1)->get()->toArray();
+        $newList  = Cache::remember('new_list', 3600, function () use ($field) {
+            return Goods::where('status', 1)->where('is_new', 1)->limit(6)->orderBy(DB::raw('RAND()'))->get($field)->toArray();
+        });
+        $bestList = Cache::remember('new_list', 3600, function () use ($field) {
+            return Goods::where('status', 1)->limit(6)->orderBy('sale', 'desc')->get($field)->toArray();
+        });
+        $recList  = Cache::remember('new_list', 3600, function () use ($field) {
+            return Goods::where('status', 1)->where('is_recommend', 1)->limit(6)->orderBy(DB::raw('RAND()'))->get($field)->toArray();
+        });
+        $saleOff  = Cache::remember('new_list', 3600, function () use ($field) {
+            return Goods::where('discount', '>', 0)->where('status', 1)->orderBy('id', 'desc')->limit(2)->get($field)->toArray();
+        });
+        $banner   = Cache::remember('new_list', 3600, function () use ($field) {
+            return Advert::with(["ad_position" => function ($query) {
+                $query->where('symbol', 'sybanner');
+            }])->where('status', 1)->get()->toArray();
+        });
         return view('index', compact("newList", "bestList", "recList", "saleOff", "banner"));
     }
 
