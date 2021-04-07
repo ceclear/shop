c4eed98e5f6cf27909dfa3742879a756849c8389
ceclear <594652523@qq.com>
2021-04-07 11:06:08 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sync-jd')->daily();
-        $schedule->command('sync:news')->daily();
+        $schedule->command('sync:news')->everyFiveMinutes();
         $schedule->command('sync:joke')->daily();
         $schedule->command('sync:today:history')->daily();
     }
