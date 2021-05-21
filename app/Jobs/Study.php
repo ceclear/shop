fdc3c31fdbd0ba47988dc8ee2596e032853b1b91
ceclear <594652523@qq.com>
2021-05-21 15:29:47 +0800

    111

--- app/Jobs/Study.php
+++ app/Jobs/Study.php
@@ -40,7 +40,7 @@ class Study implements ShouldQueue
         Mail::to('594652523@qq.com')->send(new StudyComplete($this->userInfo, $this->studyInfo));
 //        Mail::to('1414351551@qq.com')->send(new StudyComplete($this->userInfo, $this->studyInfo));
         //服务通知加一个
-        Log::info('提交作业信息了',$this->studyInfo);
+//        Log::info('提交作业信息了',$this->studyInfo);
         $app     = Factory::miniProgram(config('wechat.mini_program.default'));
         $content = '总数:' . $this->studyInfo['total'] . ',用时:' . $this->studyInfo['remind'] . ',正确数:' . $this->studyInfo['yes'] . ',错误数:' . $this->studyInfo['no'] . ',正确率:' . $this->studyInfo['rate'] . ',提交时间:'
             . $this->studyInfo['submit_time'];
@@ -48,7 +48,7 @@ class Study implements ShouldQueue
             'touser'      => 'oc53p5dwSYOIKYOgduU-7aIOZoAU',
             'template_id' => 'sNrOvfxKncoCjKZ-KM77XV6y8vrTUgWK98wwOV2L4S4',
             'data'        => [
-                'thing1' => "$this->userInfo['nickname']",
+                'thing1' => "{$this->userInfo['nickname']}",
                 'thing2' => '1.12',
                 'thing3' => '连加连减计算',
                 'thing4' => $content
