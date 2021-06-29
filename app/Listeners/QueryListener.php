ff41875e4128445d1bb634e7a793c745a2f28ee5
ceclear <594652523@qq.com>
2021-06-29 10:40:56 +0800

    log

--- app/Listeners/QueryListener.php
+++ app/Listeners/QueryListener.php
@@ -2,9 +2,7 @@
 
 namespace App\Listeners;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Database\Events\QueryExecuted;
-use Illuminate\Queue\InteractsWithQueue;
 
 class QueryListener
 {
@@ -25,18 +23,19 @@ class QueryListener
     {
         //
 //        if (env('APP_ENV', 'production') == 'local') {
-            $sql = str_replace('?', "'%s'", $event->sql);
-            $log = vsprintf($sql, $event->bindings);
-            $this->put_log('sql', $log);
+        $sql = str_replace('?', "'%s'", $event->sql);
+        $log = vsprintf($sql, $event->bindings);
+        $this->put_log('sql', $log);
 //        }
     }
 
     private function put_log($file = 'app', $content = '')
     {
-        $data = date('Y-m-d');
+        $data     = date('Y-m-d');
         $cut_line = str_repeat("-", 100);
-        is_dir(storage_path('logs/sql')) or mkdir (storage_path('logs/sql'), 0777, true); // 文件夹不存在则创建
-        $content = '[' . date('Y-m-d H:i:s') . "]" . $content;
-        @file_put_contents(storage_path('logs/sql/' . $file . '-' . $data . '.log'), $content . "\n" . $cut_line . "\n\n", FILE_APPEND);
+        is_dir(storage_path('logs/sql')) or mkdir(storage_path('logs/sql'), 0777, true); // 文件夹不存在则创建
+        $content  = '[' . date('Y-m-d H:i:s') . "]" . $content;
+        $fileName = $file . '-' . $data . '.log';
+        @file_put_contents(storage_path('logs/sql/' . $fileName), $content . "\n" . $cut_line . "\n\n", FILE_APPEND);
     }
 }
