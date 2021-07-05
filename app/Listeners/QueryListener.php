4a6b05858b507d46b8e0985fa5311536ffc383b1
ceclear <594652523@qq.com>
2021-07-05 09:46:30 +0800

    asda

--- app/Listeners/QueryListener.php
+++ app/Listeners/QueryListener.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\Cache;
 
 class QueryListener
 {
@@ -36,9 +37,12 @@ class QueryListener
         is_dir(storage_path('logs/sql')) or mkdir(storage_path('logs/sql'), 0777, true); // 文件夹不存在则创建
         $content  = '[' . date('Y-m-d H:i:s') . "]" . $content;
         $fileName = $file . '-' . $data . '.log';
-//        if (file_exists(storage_path('logs/sql/' . $fileName))) {
-//            chmod(storage_path('logs/sql/' . $fileName), 0777);
-//        }
+        $cacheKey = $file . '-' . $data;
+        $expire   = strtotime("+1 day", strtotime($data)) - 1 - time();
+        if (!Cache::has($cacheKey) && file_exists(storage_path('logs/sql/' . $fileName))) {
+            Cache::set($cacheKey, 1, $expire);
+            chmod(storage_path('logs/sql/' . $fileName), 0777);
+        }
         @file_put_contents(storage_path('logs/sql/' . $fileName), $content . "\n" . $cut_line . "\n\n", FILE_APPEND);
     }
 }
