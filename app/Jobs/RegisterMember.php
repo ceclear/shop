2fe0f0c879f3824b472ae720c741da65ad5dcf4e
zt <594652523@qq.com>
2024-03-08 15:24:34 +0800

    1

--- app/Jobs/RegisterMember.php
+++ app/Jobs/RegisterMember.php
@@ -3,17 +3,17 @@
 namespace App\Jobs;
 
 use App\Mail\Register;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
+//use Illuminate\Bus\Queueable;
+//use Illuminate\Contracts\Queue\ShouldQueue;
+//use Illuminate\Foundation\Bus\Dispatchable;
+//use Illuminate\Queue\InteractsWithQueue;
+//use Illuminate\Queue\SerializesModels;
+//use Illuminate\Support\Facades\Log;
+//use Illuminate\Support\Facades\Mail;
 
 class RegisterMember implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+//    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $userInfo;
 
@@ -33,7 +33,7 @@ class RegisterMember implements ShouldQueue
      */
     public function handle()
     {
-        //todo
+        //todo 取消发送验证码
         Mail::to('594652523@qq.com')->send(new Register($this->userInfo));
         Log::info('--------新注册用户----' . $this->userInfo['id'] . '邮件发送完毕');
 
